0f53c4df43d1c47bd1a42cb952cd2ec26ff796f7
EJ-KANG02 <97087532+EJ-KANG02@users.noreply.github.com>
2023-12-18 14:03:43 +0900

    Update DijkstraAlgorithm.java(비교용)

--- backend/src/main/java/com/example/backend/function/DijkstraAlgorithm.java
+++ backend/src/main/java/com/example/backend/function/DijkstraAlgorithm.java
@@ -1,104 +1,109 @@
 package com.react.roadmap.function;
 
+import org.springframework.stereotype.Component;
+
 import java.util.*;
 
+@Component
 public class DijkstraAlgorithm {
-    public List<String> findShortestPath(Node[] nodes, String startNode, String finishNode) {
-        Map<String, Double> distanceMap = new HashMap<>();
-        Map<String, String> previousNodeMap = new HashMap<>();
 
+    int visitCount = 0;
+
+    // 각 노드까지의 현재까지의 비용(gScore), 이전 노드(previousNode)를 저장하는 맵들
+    private Map<String, Double> gScoreMap = new HashMap<>();
+    private Map<String, String> previousNodeMap = new HashMap<>();
+
+    // 다익스트라 알고리즘을 통해 최단 경로를 찾는 메서드
+    public List<String> findShortestPath(Node[] nodes, String start, String finish, List<String> finishNodes) {
+        // 초기화
         for (Node node : nodes) {
-            if (node.getCode().equals(startNode)) {
-                distanceMap.put(node.getCode(), 0.0);
-            } else {
-                distanceMap.put(node.getCode(), Double.POSITIVE_INFINITY);
-            }
+            gScoreMap.put(node.getCode(), Double.POSITIVE_INFINITY);
         }
 
-        PriorityQueue<Node> priorityQueue = new PriorityQueue<>(Comparator.comparingDouble(node -> distanceMap.get(node.getCode())));
+        // 시작 노드의 초기 비용 설정
+        gScoreMap.put(start, 0.0);
 
-        for (Node node : nodes) {
-            if (node.getCode().equals(startNode)) {
-                priorityQueue.offer(node);
-                break;
+        // 방문한 노드를 저장하는 집합
+        Set<String> visitedNodes = new HashSet<>();
+
+        // 우선순위 큐가 빌 때까지 반복
+        while (true) {
+            // 현재 비용이 가장 작은 노드 선택
+            String currentNodeCode = getMinCostNode(nodes, visitedNodes);
+            visitedNodes.add(currentNodeCode);
+
+            visitCount++;
+
+            // 도착지 후보 중 하나에 도달하면 종료
+            if (finishNodes.contains(currentNodeCode)) {
+                // 도착한 노드가 도착지 후보 중 하나이면 바로 반환
+                System.out.print(visitCount + " ");
+                return reconstructPath(currentNodeCode, start);
             }
-        }
 
-        List<String> visitedNodes = new ArrayList<>();
+            // 현재 노드의 이웃 노드들을 탐색
+            for (Node.NeighborNode neighborNode : getNodeByCode(nodes, currentNodeCode).getNearNodes()) {
+                visitCount++;
+                String neighborCode = neighborNode.getCode();
+                double edgeWeight = neighborNode.getWeight();
 
-        while (!priorityQueue.isEmpty()) {
-            Node currentNode = priorityQueue.poll();
+                // 새로운 gScore 계산
+                double tentativeGScore = gScoreMap.get(currentNodeCode) + edgeWeight;
 
-            if (currentNode.getCode().equals(finishNode)) {
-                break;
-            }
-
-            visitedNodes.add(currentNode.getCode());  // Track the visited nodes
-
-            for (int i = 0; i < 8; i++) {
-                String neighborCode = getNodeProperty(currentNode, "nearNode" + i);
-                if (neighborCode != null && distanceMap.containsKey(neighborCode)) {
-                    double edgeWeight = getNodePropertyDouble(currentNode, "weight" + i);
-                    double currentDistance = distanceMap.get(currentNode.getCode());
-                    double neighborDistance = distanceMap.get(neighborCode);
-
-                    // Check if the neighbor node's centralNode value is "O"
-                    String neighborCentralNode = getNodeProperty(getNodeByCode(nodes, neighborCode), "centralNode");
-                    if (neighborCentralNode != null && neighborCentralNode.equals("O")) {
-                        if (neighborCode.equals(finishNode) || currentNode.getCode().equals(finishNode)) {
-                             // Skip this neighbor node if it's the finish node or if the current node is the finish node
-                        } else if (!currentNode.getCode().equals(startNode)) {
-                            continue; // Skip this neighbor node if it's not the start node
-                        } else{
-                            continue;
-                        }
-                    }
-
-                    double newDistance = currentDistance + edgeWeight;
-
-                    if (newDistance < neighborDistance) {
-                        distanceMap.put(neighborCode, newDistance);
-                        previousNodeMap.put(neighborCode, currentNode.getCode());
-                        priorityQueue.offer(getNodeByCode(nodes, neighborCode));
-                    }
+                // 현재까지의 최소 비용보다 작으면 업데이트
+                if (tentativeGScore < gScoreMap.get(neighborCode)) {
+                    gScoreMap.put(neighborCode, tentativeGScore);
+                    previousNodeMap.put(neighborCode, currentNodeCode);
                 }
             }
+
+            // 모든 노드를 방문한 경우 종료
+            if (visitedNodes.size() == nodes.length) {
+                break;
+            }
         }
 
-        List<String> shortestPath = new ArrayList<>();
-        String currentNodeCode = finishNode;
+        // 도착지에 도달하지 못한 경우 null 반환 또는 예외 처리 등을 추가할 수 있습니다.
+        return null;
+    }
 
+    // 현재 비용이 가장 작은 노드를 찾는 메서드
+    private String getMinCostNode(Node[] nodes, Set<String> visitedNodes) {
+        String minNode = null;
+        double minCost = Double.POSITIVE_INFINITY;
+
+        for (Node node : nodes) {
+            visitCount++;
+            String nodeCode = node.getCode();
+            if (!visitedNodes.contains(nodeCode) && gScoreMap.get(nodeCode) < minCost) {
+                minCost = gScoreMap.get(nodeCode);
+                minNode = nodeCode;
+            }
+        }
+
+        return minNode;
+    }
+
+    // 최단 경로를 직접 추적하는 메서드
+    private List<String> reconstructPath(String lastNodeCode, String startNodeCode) {
+        List<String> shortestPath = new ArrayList<>();
+        String currentNodeCode = lastNodeCode;
+
+        // 역으로 추적하여 최단 경로 찾기
         while (currentNodeCode != null) {
             shortestPath.add(currentNodeCode);
+            if (currentNodeCode.equals(startNodeCode)) {
+                break;
+            }
             currentNodeCode = previousNodeMap.get(currentNodeCode);
         }
 
-        Collections.reverse(shortestPath);  // Reverse the list to get the correct order
-
-
+        // 경로 뒤집기
+        Collections.reverse(shortestPath);
         return shortestPath;
     }
 
-
-
-    private String getNodeProperty(Node node, String propertyName) {
-        try {
-            return (String) node.getClass().getMethod("get" + capitalizeFirstLetter(propertyName)).invoke(node);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    private double getNodePropertyDouble(Node node, String propertyName) {
-        try {
-            return (double) node.getClass().getMethod("get" + capitalizeFirstLetter(propertyName)).invoke(node);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return 0.0;
-        }
-    }
-
+    // 코드에 해당하는 노드 찾기
     private Node getNodeByCode(Node[] nodes, String code) {
         for (Node node : nodes) {
             if (node.getCode().equals(code)) {
@@ -106,9 +111,5 @@ public class DijkstraAlgorithm {
             }
         }
         return null;
-    }
-
-    private String capitalizeFirstLetter(String str) {
-        return str.substring(0, 1).toUpperCase() + str.substring(1);
     }
 }
