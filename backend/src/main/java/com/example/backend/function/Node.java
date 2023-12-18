102ef49d70fd991cf62646158fd354a2f5f053ed
EJ-KANG02 <97087532+EJ-KANG02@users.noreply.github.com>
2023-12-18 13:36:16 +0900

    Update Node.java

--- backend/src/main/java/com/example/backend/function/Node.java
+++ backend/src/main/java/com/example/backend/function/Node.java
@@ -1,9 +1,12 @@
 package com.react.roadmap.function;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
@@ -14,33 +17,30 @@ public class Node {
     private String centralNode;
     private String latitude;
     private String longitude;
-    private String nearNode0;
-    private double weight0;
-    private String nearNode1;
-    private double weight1;
-    private String nearNode2;
-    private double weight2;
-    private String nearNode3;
-    private double weight3;
-    private String nearNode4;
-    private double weight4;
-    private String nearNode5;
-    private double weight5;
-    private String nearNode6;
-    private double weight6;
-    private String nearNode7;
-    private double weight7;
+    private List<NeighborNode> nearNodes;
+
+    // 추가된 내용: 휴리스틱 정보를 저장할 List<HeuristicNode> 필드
+    private List<HeuristicNode> heuristic;
+
+    // 추가된 내용: 이웃 노드를 나타내는 클래스
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class NeighborNode {
+        private String code;
+        private double weight;
+    }
+
+    // 추가된 내용: 휴리스틱 노드를 나타내는 클래스
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class HeuristicNode {
+        private String code;
+
+        @JsonProperty("h-weight") // JSON 데이터의 키와 매핑
+        private double hWeight;
+    }
 }
-
-// public class nearNode {
-//     private String code;
-//     private double weight;
-// }
-
-// public class Node {
-//     private String code;
-//     private String centralNode;
-//     private String latitude;
-//     private String longitude;
-//     private List<nearNode> nearNodes;
-// }
